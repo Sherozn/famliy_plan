d6594c818dde838cfe9a2ce19b4f82ae81ae1aa0
xzn <1369822836@qq.com>
2019-10-31 04:06:25 +0000

    push

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module DataSystemText
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W(#{config.root}/lib)
-    config.i18n.default_locale = :'zh-CN'
+    # config.i18n.default_locale = :'zh-CN'
   end
 end
